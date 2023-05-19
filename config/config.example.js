2d9dc86b0c681fb73f1c92fba64e4f2c3d6f0ade
fxhxyz4 <fxhxyz@proton.me>
2023-05-19 08:13:12 +0300

    edit example config

--- config/config.example.js
+++ config/config.example.js
@@ -2,5 +2,8 @@ export const config = {
   user: "username",
   token: "oauth:token",
   channels: ["#channel", "#channel2", "#channel3"],
-  message: "lol", // spam message
+  spam: false,
+  message: "Tasty ",
+  repeat: 50,
+  count: 20,
 };
