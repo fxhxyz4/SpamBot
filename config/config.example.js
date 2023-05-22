b23535b36e21f4768938fa981a351577819e2b24
fxhxyz4 <fxhxyz@proton.me>
2023-05-23 01:41:52 +0300

    edit config

--- config/config.example.js
+++ config/config.example.js
@@ -2,6 +2,8 @@ export const config = {
   user: "username",
   token: "oauth:token",
   channels: ["#channel", "#channel2", "#channel3"],
+  ban: false,
+  blist: ["word1", "word2"],
   spam: false,
   message: "Tasty ",
   repeat: 50,
